4777d19f3e7c4c5eca406198508fe80fd23e46db
Paul Hu <paul.h@lifebyte.io>
2021-01-05 16:58:01 +0800

    HUBX-149, HUBX-150, HUBX-266: update to mam and hubx account to cache aside pattern

--- src/main/java/com/lifebyte/hubx/admin/mapper/InstrumentMapper.java
+++ src/main/java/com/lifebyte/hubx/admin/mapper/InstrumentMapper.java
@@ -1,8 +1,7 @@
 package com.lifebyte.hubx.admin.mapper;
 
-import com.lifebyte.hubx.data.entity.account.TradingCurrencyVO;
-import com.lifebyte.hubx.data.entity.account.TradingLeverageVO;
-import com.lifebyte.hubx.data.entity.symbol.SymbolDisplayNameVO;
+import com.lifebyte.hubx.data.entity.account.HubxCurrencyVO;
+import com.lifebyte.hubx.data.entity.account.HubxLeverageVO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,8 +9,8 @@ import java.util.List;
 @Mapper
 public interface InstrumentMapper {
 
-    List<TradingCurrencyVO> queryAllCurrencies();
+    List<HubxCurrencyVO> queryAllCurrencies();
 
-    List<TradingLeverageVO> queryAllLeverages();
+    List<HubxLeverageVO> queryAllLeverages();
 
 }
