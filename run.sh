4d1c2f59f05bc81c973a68c9410947e0d41fbd7a
Zhichao Zheng <chao@Zhichaos-MacBook-Pro.local>
2021-10-06 20:26:00 +1100

    save

---
+++ run.sh
@@ -0,0 +1,2 @@
+export SPRING_CONFIG_LOCATION=/Users/chao/Work/lifebyte/HUBX/Library/hubx-core-1-common/common-config/src/main/resources/common.yml,/Users/chao/Work/lifebyte/HUBX/Library/hubx-core-2-admin/src/main/resources/application.yml
+java -jar ./target/hubx-core-2-admin-docker.jar
